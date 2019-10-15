7708fb94a20f2e196be1bd357edda1fca5a9bb9e
Sashidhar Jakkamsetti <sashidhar.jakkamsetti@gmail.com>
2019-10-15 15:11:45 -0700

    hw1 part3 done

--- hw1/main.c
+++ hw1/main.c
@@ -8,9 +8,36 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
+int a[32];
+
+unsigned long sum(int n) {
+    int i;
+    unsigned long sum = 0;
+
+    for (i = 0; i < n; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
+}
+
+unsigned long crash_array(int n) {
+    int i;
+    unsigned long sum = 0;
+
+    for (i = 0; i < n && i < sizeof(a)/sizeof(a[0]); i++) {
+        sum = sum + a[i];
+    }
+
+    return sum;
+}
+
 int main(void) {
-    
-    printf("Hello world\n"); 
-    return 0; 
+
+    unsigned long s;
+
+    s = crash_array(100000);
+    printf("crash array sum:%ld\n", s);  
+    return 0;
 }
 
