cf20718a8c0abb5e139b918502ecb6eec5b914ca
Sashidhar Jakkamsetti <sjakkams@circinus-22.ics.uci.edu>
2019-10-28 19:09:52 -0700

    still debugging

--- hw2/main.c
+++ hw2/main.c
@@ -64,29 +64,42 @@ int main(int argc, char* argv[]) {
     void *entry = NULL;
     void *code_va = NULL;
     int ret; 
-
-    FILE* file = fopen("elf", "rb");
+    int i;
     
-    if(file) {
+    printf("argv[1] = %s\n", argv[1]);
+    FILE* file = fopen(argv[1], "r");
+    
+    if(file != NULL) {
       // 1. Read the ELF header
       fread(&elf, 1, sizeof(elf), file);
+      
+      if(elf.magic == ELF_MAGIC) {
+	    printf("step 1\n");
 
-      if(elf.magic == ELF_MAGIC &&
-         elf.elf[0] == 'E' &&
-         elf.elf[1] == 'L' &&
-         elf.elf[2] == 'F' &&
-         elf.type == 0x7f) {
             // 2. Go the Program Header Table offset
-            fseek(file, elf.phoff - elf.ehsize, SEEK_SET);
+            fseek(file, elf.phoff, SEEK_SET);
+	    printf("step 2: seek set to %d\n", elf.phoff);
 
-            for(int i = 0; i < elf.phnum; i++) {
+	    // 3. Read each entry of the program header table
+            for(i = 0; i < elf.phnum; i++) {
                 fread(&ph, 1, sizeof(ph), file);
                 if(ph.type == ELF_PROG_LOAD) {
+		    printf("step 3: ph offset = %d,%d\n", ph.off, ph.memsz);
+
+		    // 4. Load segment into the memory 
+		    fseek(file, ph.off, SEEK_SET);
                     code_va = mmap(code_va, ph.memsz, 
                         PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 
                         0, 0);
-                    if(code_va != -1) {
-                      entry = code_va + elf.entry;
+		    fread(&code_va, 1, ph.memsz, file);
+		    
+		    fseek(file, elf.phoff + sizeof(ph), SEEK_SET);
+		    
+		    printf("step 4: code_va = %d\n", code_va);
+                    if(code_va != (void*)-1) {
+		      // 5. Jump to the entry point of the program
+                      entry = code_va - 72;
+		      printf("step 5: entry = %d\n", entry);
                     }
                 }
             }
@@ -98,7 +111,8 @@ int main(int argc, char* argv[]) {
         ret = sum(1, 2);
         printf("sum:%d\n", ret); 
     };
-
+    
+    return 0;
 
 }
 
